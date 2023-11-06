3d657a0a6b0081511583d37089fae748dc3a6068
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-11-06 22:34:07 +0100

    added normalize and delete description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <form className="form js-form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">KANTOR</legend>
+                <p>
+                    <label>
+                        <span className="form__labelText">
+                            Kwota w PLN* </span>
+                        <input className="form__field js-amount" placeholder="podaj kwotę" type="number" min="0.1" required
+                            step="0.01">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">
+                            Waluta
+                        </span>
+                        <select className="form__field js-currency" required>
+                            <option name="EUR">EUR</option>
+                            <option name="USD">USD</option>
+                            <option name="JPY">JPY</option>
+                        </select>
+                    </label>
+                </p>
+            </fieldset>
+            <p>
+                <button className="form__button">Przelicz</button>
+            </p>
+        </form>
+        <p className="container__paragraph">Po przeliczeniu otrzymasz <strong className="js-result">N/A</strong></p>
     </div>
   );
 }
