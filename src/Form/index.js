77a348ba90f22727148a8f4212c70589231d02c9
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-11-13 10:18:14 +0100

    add new currencies

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,33 +1,47 @@
 import "./style.css";
+import currencies from "../currencies";
+import { useState } from "react";
 
-const Form = () => {
+const Form = ({ calculateResult }) => {
+    const [currency, setCurrency] = useState("");
+    const [amount, setAmount] = useState("");
+  
+    const onFormSubmit = (event) => {
+      event.preventDefault();
+      calculateResult(currency, amount);
+    };
+
     return (
-        <form className="form">
-            <fieldset className="form__fieldset">
+        <form className="form" onSubmit={onFormSubmit}>
+            <fieldset className="">
                 <legend className="form__legend">KANTOR</legend>
                 <p>
                     <label>
                         <span className="form__labelText">
                             Kwota w PLN* </span>
-                        <input
+                        <input 
                         className="form__field"
                         placeholder="podaj kwotę"
                         type="number"
                         min="0.1"
-                        required step="0.01" />
+                        required step="0.01"
+                        onChange={(event) => setAmount(event.target.value)} />
                     </label>
                 </p>
                 <p>
-                    <label>
-                        <span className="form__labelText">
-                            Waluta
-                        </span>
-                        <select className="form__field" required>
-                            <option name="EUR">EUR</option>
-                            <option name="USD">USD</option>
-                            <option name="JPY">JPY</option>
-                        </select>
-                    </label>
+                <label className="form__label">
+            Waluta
+            <select
+              className="form__field"
+              onChange={(event) => setCurrency(event.target.value)}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.description} value={currency.name}>
+                  {currency.name}
+                </option>
+              ))}
+            </select>
+          </label>
                 </p>
             </fieldset>
             <p>
---
+++ src/currencies.js
@@ -0,0 +1,16 @@
+const currencies = [
+    {
+      name: "JPY",
+      rate: 3.32,
+    },
+    {
+      name: "USD",
+      rate: 4.32,
+    },
+    {
+      name: "EUR",
+      rate: 4.71,
+    },
+  ];
+
+export default currencies;
