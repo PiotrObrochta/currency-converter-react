a2d41b99b0221e7dba572c8eee0e301ff9449cd3
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-12-11 18:47:42 +0100

    add dateFormat timeFormat

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,5 +1,36 @@
 import "./style.css";
+import { useState, useEffect } from 'react';
 
-const Clock = () => {};
+const Clock = () => {
+    const dateFormat = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    };
 
-export default Clock; 
+    const timeFormat = {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    };
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <div className="clock">
+            <p>
+            Dzisiaj jest {date.toLocaleString(undefined, dateFormat)}, {date.toLocaleString(undefined, timeFormat)}
+            </p>
+        </div>
+    );
+};
+
+export default Clock;
